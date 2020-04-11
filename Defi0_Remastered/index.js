15d304c0ca19f4a35a5c8c67ec40a2ed085d8eb6
FabriceT_Dev <fabrice.tapia95@gmail.com>
2020-04-11 23:05:22 +0200

    Defi0 - Serveur de requête de block

--- Defi0_Remastered/index.js
+++ Defi0_Remastered/index.js
@@ -9,13 +9,12 @@ const port = 8080
 
 
 app.get('/', (req, resp)=>{
-	resp.send("bojour");
+	resp.send("bonjour");
 })
 
 
 app.get('/block/:block',(requete,reponse)=>{
-    console.log(`vous avez demandé le block ${requete.params.block}`)
-    //reponse.send(`vous avez demandé le block ${requete.params.block}`)
+    console.log(`Vous avez demandé le block ${requete.params.block}`)
     const url = "https://blockchain.info/rawblock/"+requete.params.block
     fetch(url)
     .then(res => res.json())
@@ -29,28 +28,5 @@ app.get('/block/:block',(requete,reponse)=>{
     })
     	
 })
-
-/*
-
-app.get('/block/:block', (req, resp)=>{
-	 console.log(`Vous avez demandé le block ${req.params.block}`)
-	 const url = "https://blockchain.info/rawblock/"+req.params.block;
-	 fetch(url)
- 	.then(r => console.log(r.json()))
-	 //.then(json=> {
-	 	//resp.write("La date :", json.time);
-	 	//resp.write("La hauteur :", json.height)
-	 	/*tabIdHash = []
-	 	for(i=0; i < json.n_tx; i++){
-	 		tabIdHash.push(json.tx[i].hash)
-	 	}
-
-	 	resp.sendFile("La date: ", json.time , ". Sa hauteur: ", json.height, ". Ses id sont les suivants:", tabIdHash)*/
-
-	 //	console.log("bonjour")
-	// })
-
-//})
-
 
 app.listen(port, () => console.log(`le serveur est lancé sur http://localhost:${port} !`));
